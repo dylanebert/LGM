9e2ba1ccee3c22a22a38a2f77cf0b90fe3496d69
dylanebert <dylan@huggingface.co>
2024-04-09 21:18:22 -0400

    minimal lgm module

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 import numpy as np
-import rembg
 import torch
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
-from safetensors.torch import load_file
+from diffusers import DiffusionPipeline
 
-from lgm import LGM
 from multiview import image_to_multiview
-
-model = LGM().half().to("cuda")
-ckpt = load_file("pretrained/model_fp16.safetensors", device="cpu")
-model.load_state_dict(ckpt, strict=False)
 
 IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
 
-bg_remover = rembg.new_session()
 
 images = image_to_multiview(
     "https://huggingface.co/datasets/dylanebert/3d-arena/resolve/main/cat_statue.jpg",
     "",
     True,
 )
+
 
 images = np.stack([images[1], images[2], images[3], images[0]], axis=0)
 images = images.astype(np.float32) / 255.0
@@ -34,11 +28,17 @@ images = F.interpolate(
 )
 images = TF.normalize(images, IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)
 
-rays_embeddings = model.prepare_default_rays("cuda", elevation=0)
-input_image = torch.cat([images, rays_embeddings], dim=1).unsqueeze(0)
+pipeline = DiffusionPipeline.from_pretrained(
+    "dylanebert/LGM",
+    custom_pipeline="dylanebert/LGM",
+    torch_dtype=torch.float16,
+    trust_remote_code=True,
+).to("cuda")
 
-with torch.no_grad():
-    with torch.autocast(device_type="cuda", dtype=torch.float16):
-        gaussians = model.forward(input_image)
+rays_embeddings = pipeline.prepare_default_rays("cuda", elevation=0)
+inputs = torch.cat([images, rays_embeddings], dim=1).unsqueeze(0)
 
-    model.gs.save_ply(gaussians, "output/gaussians.ply")
+with torch.autocast(device_type="cuda", dtype=torch.float16):
+    result = pipeline(inputs)
+
+pipeline.gs.save_ply(result, "output/gaussians.ply")
